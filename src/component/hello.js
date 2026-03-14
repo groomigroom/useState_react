aaf0f50d82b4abc8ee9177c73566f7cfbe7079ba
groomigroom <googjohn0143@gmail.com>
2026-03-14 23:41:41 +0000

    hello_part

--- src/component/hello.js
+++ src/component/hello.js
@@ -1,1 +1,12 @@
-export default function Hello() { }
+import { useState } from "react";
+
+export default function Hello() {
+    
+    const [name, setName] = useState("groomi");
+
+    return (
+        <div>
+            <h2 id="name"></h2>
+        </div>
+    );
+}
